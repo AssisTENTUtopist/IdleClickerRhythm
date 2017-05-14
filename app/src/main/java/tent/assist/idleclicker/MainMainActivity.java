180edf2a64b3982424000eba716a30486ef43ffe
AssisTENTUtopist <maksim.aleksandr2000@mail.ru>
2017-05-14 23:07:04 +0500

    Making game of clickers out of it

---
+++ app/src/main/java/tent/assist/idleclicker/MainMainActivity.java
@@ -0,0 +1,18 @@
+package tent.assist.idleclicker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainMainActivity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+
+    public void startBarGame (View view) {
+        Intent intent = new Intent(this, SenseOfBarMainActivity.class);
+        startActivity(intent);
+    }
+}
